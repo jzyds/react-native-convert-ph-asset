5b50455cec324994b4fa6733776bd74c04ff71cc
xqli <xiaoqiangli.mail@gmail.com>
2019-09-08 21:23:11 +0800

    convertVideoFromId convertVideoFromUrl

--- index.js
+++ index.js
@@ -3,4 +3,9 @@ import { NativeModules } from 'react-native';
 
 const { RNConvertPhAsset } = NativeModules;
 
+RNConvertPhAsset.convertVideoFromUrl = (url) => {
+  const id = url.substring (5, 41);
+  return RNConvertPhAsset.convertVideoFromId(id)
+}
+
 export default RNConvertPhAsset;
