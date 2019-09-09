0d71df95a7fbc0bcb01d5ef04d7626d07b39092c
xqli <xiaoqiangli.mail@gmail.com>
2019-09-09 14:34:04 +0800

    update

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+import {NativeModules, Platform} from 'react-native';
+let RNConvertPhAsset = {};
 
-import { NativeModules } from 'react-native';
-
-const { RNConvertPhAsset } = NativeModules;
-
-RNConvertPhAsset.convertVideoFromUrl = (map) => {
-  map.id = map.url.substring (5, 41);
-  return RNConvertPhAsset.convertVideoFromId(map)
+if (Platform.OS === 'ios') {
+  RNConvertPhAsset = NativeModules.RNConvertPhAsset;
+  RNConvertPhAsset.convertVideoFromUrl = map => {
+    map.id = map.url.substring (5, 41);
+    return RNConvertPhAsset.convertVideoFromId (map);
+  };
 }
 
 export default RNConvertPhAsset;
